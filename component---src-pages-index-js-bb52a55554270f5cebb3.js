916b701ec225e84e4fe20474c10b9449754f6eee
Travis CI User <travis@example.org>
2020-04-07 15:53:08 +0000

    Updates

--- component---src-pages-index-js-bb52a55554270f5cebb3.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{RXBc:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a);t.default=function(){return l.a.createElement(l.a.Fragment,null,l.a.createElement("h1",null,"Notes2Self"),l.a.createElement("p",null,"No shit notes, how-to's, pointers, tips, and tricks."),l.a.createElement("p",null,"© Joseph R Miles 2020"))}}}]);
-//# sourceMappingURL=component---src-pages-index-js-bb52a55554270f5cebb3.js.map
